4c200a4683f7625cb63cb9c38c44cb7c2e68e006
David Boultbee <dboultbee@gmail.com>
2014-08-23 17:39:54 +0100

    m

--- scripts.main.js
+++ scripts.main.js
@@ -36,12 +36,15 @@ $(document).ready(function(){
 			
 			var oGalleryImages=new function(){
 				var _oGalleryImages=this,
-					aGalleryImages=new Array(),
+					aGalleryImages={};//new Array(),
 					iOpenScans=0,
-					oFileSystem;	
+					sStorageDir='dcim/slyncstagram';	
 					
 				function fFinishedScanning(){
 					console.log('filesystem: finished scan');
+					
+					console.log(JSON.stringify(aGalleryImages).replace(/,/g,',\n'));
+					
 					$(_oGalleryImages).trigger('ready');
 				}
 			
@@ -64,7 +67,11 @@ $(document).ready(function(){
 					
 							if(!aItems[i].name.match(/\.jpg$/i)) continue;
 				
-							aImageFiles.push(aItems[i]);
+							//aImageFiles.push(aItems[i]);
+							aImageFiles.push({
+								name:aItems[i].name,
+								path:aItems[i].toURL()
+							});
 						}
 						
 						//console.log('filesystem: found '+aImageFiles.length+' images in '+sDirName);
@@ -85,10 +92,8 @@ $(document).ready(function(){
 					});
 				}	// end of fReadDirectory
 				
-				window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(_oFileSystem){
-					oFileSystem=_oFileSystem;
-					
-					oFileSystem.root.getDirectory('dcim/slyncstagram',{create:false}, function(oDirectory){
+				window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(oFileSystem){
+					oFileSystem.root.getDirectory(sStorageDir,{create:false}, function(oDirectory){
 						//console.log('filesystem: got initial directory');
 						var sDirName=oDirectory.name;
 						fReadDirectory(oDirectory);
@@ -108,12 +113,18 @@ $(document).ready(function(){
 						var sGroupName=aAllGroups[i];
 						//console.log('filesystem: extracting images from group '+sGroupName);
 						
+						if(!aGalleryImages[sGroupName].images) continue;
+						
 						var aGroupImages=aGalleryImages[sGroupName].images;
 						//console.log('filesystem: extracted '+aGroupImages.length+' images from group '+sGroupName);
 						for(var j=0;j<aGroupImages.length;j++) aAllImages.push(aGroupImages[j]);
 					}
-					return aAllImages;
+					return aAllImages;//.shuffle();
 				}		
+				
+				_oGalleryImages.listImages=function(){
+					return aGalleryImages;
+				}
 				
 			} // end of gallery images object
 			
@@ -121,42 +132,56 @@ $(document).ready(function(){
 				console.log('gallery: beginning');
 				
 				var iCurrent=0,
-					aImageFiles=oGalleryImages.allImages();
+					aImageFiles=oGalleryImages.listImages()['Nature'].images,//.allImages();
 					iMax=aImageFiles.length;
 					
 				console.log('gallery: there are '+aImageFiles.length+' images in the gallery');
 
 				function fNextImg(){
+					if(iCurrent==7) iCurrent++;
+				
 					var _iCurrent=iCurrent;
 					console.log('gallery: working with image '+_iCurrent);
-					aImageFiles[_iCurrent].file(function(oFile){
-						console.log('gallery: opened image '+_iCurrent);
-						var reader = new FileReader();
-						reader.onloadend = function(evt) {
-							console.log('gallery: loaded image '+_iCurrent);
-							var oImg=document.createElement('IMG');
-							oImg.onload=function(){
-								var _oImg=this,
-									oImgHolder=document.createElement('DIV');
+					console.log('gallery: image is on path '+aImageFiles[_iCurrent].path);
+					
+					window.resolveLocalFileSystemURL(aImageFiles[_iCurrent].path,function(oFileEntry){
+						console.log('gallery: found image '+_iCurrent);
+						
+						oFileEntry.file(function(oFile){
+							console.log('gallery: opened image '+_iCurrent);
+							var reader = new FileReader();
+							reader.onloadend = function(evt) {
+								console.log('gallery: loaded image '+_iCurrent);
 								
-								$(oImgHolder).addClass('imgHolder');
-								$(oImgHolder).append(_oImg);
-								$(oCarousel).append(oImgHolder);
+								//$(oCarousel).empty();
 								
-								fSizeImage(_oImg,oImgHolder,true,false);
-								console.log('gallery: showing image '+_iCurrent);
-								console.log('gallery: finished with image '+_iCurrent);
-								$(oImgHolder).prevAll().remove();
-								setTimeout(function(){
-									fNextImg();
-								},2000);								
-							};
-							$(oImg).attr('src',evt.target.result);						
-						}; 
-						reader.readAsDataURL(oFile);
+								var oImg=document.createElement('IMG');
+								oImg.onload=function(){
+									var _oImg=this,
+										oImgHolder=document.createElement('DIV');
+								
+									$(oImgHolder).addClass('imgHolder');
+									$(oImgHolder).append(_oImg);
+									$(oCarousel).append(oImgHolder);
+								
+									fSizeImage(_oImg,oImgHolder,true,false);
+									console.log('gallery: showing image '+_iCurrent);
+									console.log('gallery: finished with image '+_iCurrent);
+									$(oImgHolder).prevAll().remove();
+									setTimeout(function(){
+										fNextImg();
+									},500);							
+								};
+								$(oImg).attr('src',evt.target.result);						
+							}; 
+							reader.readAsDataURL(oFile);
+						}, function(){
+							console.log("ERROR: gallery: can't open file");
+							fNextImg();	
+						});
 					
 					},function(){
-						console.log("ERROR: gallery: can't open file");
+						console.log("ERROR: gallery: can't find file");
 						fNextImg();	
 					});
 					
@@ -166,42 +191,6 @@ $(document).ready(function(){
 				fNextImg();	
 			});
 			
-			/*
-			 // success get file system
-				
-				console.log('filesystem: got file system');
-				fileSystem.root.getDirectory('dcim/slyncstagram',{create:false}, function(oDirectory){
-					
-					console.log('gallery: got root directory');
-					 // success get files and folders
-						
-						console.log('gallery: there are '+aEntries.length+' files');
-						var aImageFiles=new Array();
-						
-						for(var i=0; i<aEntries.length; ++i) {
-							if(!aEntries[i].isFile) continue;
-							if(!aEntries[i].name.match(/\.jpg$/)) continue;
-							
-							aImageFiles.push(aEntries[i]);
-						}
-						
-						console.log('gallery: there are '+aImageFiles.length+' image .jpg files');
-						
-											
-
-					}, function(oError){ // error get files and folders
-						alert('ERROR: gallery: cannot open files and folders',oError.code);
-					});
-			
-				}, function(oError){
-					alert('ERROR: gallery: cannot create directory reader',oError.code);
-				});
-			}, function(evt){ // error get file system
-				alert('ERROR: gallery: could not access file system');
-			});
-			
-			
-			*/
 			/*
 				END OF THE IMAGE CAROUSEL
 			*/
